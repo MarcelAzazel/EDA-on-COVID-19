71264584a64456c1125b60a41b899263aa75037b
Marcel <119822011+MarcelAzazel@users.noreply.github.com>
2024-11-11 20:13:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ import matplotlib.dates as mdates
 full_grouped = pd.read_csv(r"C:\Arquivos\Coding\Projects\EDA Covid-19\WHO-COVID-19-global-daily-data.csv")
 
 china = full_grouped[full_grouped["Country"] == "China"]
+
+# Removing outliers at the end of 2022 & beginning of 2023
+china_in = china[(china["Date_reported"] < "2022-12-01") | (china["Date_reported"] > "2023-02-01")]
